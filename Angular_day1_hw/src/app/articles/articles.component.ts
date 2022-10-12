75d1a09d1bb92124a0376537db60d3ab8fab3eea
Craig Wilson <cw66786@gmail.com>
2022-10-11 21:36:41 -0500

    done besides media queries

--- Angular_day1_hw/src/app/articles/articles.component.ts
+++ Angular_day1_hw/src/app/articles/articles.component.ts
@@ -1,3 +1,4 @@
+import { preserveWhitespacesDefault } from '@angular/compiler';
 import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
@@ -9,9 +10,36 @@ export class ArticlesComponent implements OnInit {
  @Input() colors!: string[];
  @Input() colorChange!: (arg: string) => void;
 
+ shadowColor: string = '';
+ prevParent: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setShadow=(obj: any,color: string)=>{
+    
+      this.shadowColor = '5px 10px '+ color;
+      let parent: any = document.getElementById(obj.parentNode.id.toString());
+      
+
+      
+
+      parent.setAttribute("style", `border: 2px solid ${color}; box-shadow: ${this.shadowColor}`);
+
+      if(this.prevParent !== undefined && this.prevParent !== parent){
+        this.prevParent.setAttribute("style", `border: none; box-shadow: none`);
+
+      }
+
+
+     this.prevParent = parent;
+
+      
+     
+     
+      
   }
 
 }
